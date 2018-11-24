a98bf9fd9c61762dac6deca5398af9ca70d92791
Santiago <sgobotta@gmail.com>
2018-11-24 15:41:17 -0300

    Rest Client: Wraps the main arrow function into an object attribute

--- src/vuex-crud/adapter/restClient.js
+++ src/vuex-crud/adapter/restClient.js
@@ -32,7 +32,7 @@ function getIdKey({ resource, options }) {
   return (options[resource] && options[resource].id) || options.id || 'id';
 }
 
-export default (client, options = {}) => {
+export default (client, options = { rootUrl: '' }) => {
   // const usePatch = !!options.usePatch;
   const mapRequest = (type, resource, params) => {
     // const idKey = getIdKey({ resource, options });
@@ -48,54 +48,45 @@ export default (client, options = {}) => {
     });
 
     switch (type) {
-      // case GET_MANY:
-      //   const ids = params.ids || [];
-      //   query[idKey] = { $in: ids };
-      //   query.$limit = ids.length;
-      //   return service.find({ query });
-      // case GET_MANY_REFERENCE:
-      //   if (params.target && params.id) {
-      //     query[params.target] = params.id;
-      //   }
       case FETCH_LIST:
       // parseList is not being used at the moment - sgobotta
         return client.get(url);
-      // case FETCH_LIST:
-      //   const { page, perPage } = params.pagination || {};
-      //   const { field, order } = params.sort || {};
-      //   if (perPage && page) {
-      //     query.$limit = perPage;
-      //     query.$skip = perPage * (page - 1);
-      //   }
-      //   if (order) {
-      //     query.$sort = {
-      //       [field === 'id' ? idKey : field]: order === 'DESC' ? -1 : 1,
-      //     };
-      //   }
-      //   Object.assign(query, fetchUtils.flattenObject(params.filter));
-      //   return service.find({ query });
-      // case FETCH_SINGLE:
-      //   return service.get(params.id);
-      // case UPDATE:
-      //   if (usePatch) {
-      //     const data = params.previousData // use diff
-      //     return service.patch(params.id, data);
-      //   }
-      //   return service.update(params.id, params.data);
-      // case UPDATE_MANY:
-      //   if (usePatch) {
-      //     const data = params.previousData // use diff
-      //     return Promise.all(params.ids.map(id => (service.patch(id, data))));
-      //   }
-      //   return Promise.all(params.ids.map(id => (service.update(id, params.data))));
-      // case CREATE:
-      //   return service.create(params.data);
-      // case DESTROY:
-      //   return service.remove(params.id);
-      // case DELETE_MANY:
-      //   return Promise.all(params.ids.map(id => (service.remove(id))));
+        // case FETCH_LIST:
+        //   const { page, perPage } = params.pagination || {};
+        //   const { field, order } = params.sort || {};
+        //   if (perPage && page) {
+        //     query.$limit = perPage;
+        //     query.$skip = perPage * (page - 1);
+        //   }
+        //   if (order) {
+        //     query.$sort = {
+        //       [field === 'id' ? idKey : field]: order === 'DESC' ? -1 : 1,
+        //     };
+        //   }
+        //   Object.assign(query, fetchUtils.flattenObject(params.filter));
+        //   return service.find({ query });
+      case FETCH_SINGLE:
+        return client.get(`${url}/${params.id}`);
+        // case UPDATE:
+        //   if (usePatch) {
+        //     const data = params.previousData // use diff
+        //     return service.patch(params.id, data);
+        //   }
+        //   return service.update(params.id, params.data);
+        // case UPDATE_MANY:
+        //   if (usePatch) {
+        //     const data = params.previousData // use diff
+        //     return Promise.all(params.ids.map(id => (service.patch(id, data))));
+        //   }
+        //   return Promise.all(params.ids.map(id => (service.update(id, params.data))));
+      case CREATE:
+        return client.post(url, params.data);
+        // case DESTROY:
+        //   return service.remove(params.id);
+        // case DELETE_MANY:
+        //   return Promise.all(params.ids.map(id => (service.remove(id))));
       default:
-        return Promise.reject(`Unsupported FeathersJS restClient action type ${type}`);
+        return Promise.reject(`Unsupported Axios restClient action type ${type}`);
     }
   };
 
@@ -106,28 +97,18 @@ export default (client, options = {}) => {
       case UPDATE:
       case DESTROY:
         return { data: { ...response, id: response[idKey] } };
-      // case UPDATE_MANY:
-      // case DELETE_MANY:
-        // return { data: response.map(record => record[idKey]) };
+
       case CREATE:
         return { data: { ...params.data, ...response, id: response[idKey] } };
-      // case GET_MANY_REFERENCE: // fix GET_MANY_REFERENCE missing id
-      // case GET_MANY: // fix GET_MANY missing id
-      //   case GET_LIST:
-      //     response.data = response.data.map((_item) => {
-      //       const item = _item;
-      //       if (idKey !== 'id') {
-      //         item.id = _item[idKey];
-      //       }
-      //       return _item;
-      //     });
-      //     return response;
+
       default:
         return response;
     }
   };
 
-  return (type, resource, params) =>
-    mapRequest(type, resource, params)
-      .then(response => mapResponse(response, type, resource, params));
+  return {
+    fetch: (type, resource, params) =>
+      mapRequest(type, resource, params)
+        .then(response => mapResponse(response, type, resource, params))
+  };
 };
